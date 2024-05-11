6f9396a0eb475a570d4711b46a1b343097735065
aolsson00 <157762127+aolsson00@users.noreply.github.com>
2024-05-10 21:56:59 -0400

    Update input_ticket_form.php

--- Projects/Web-enabled Database Systems/Airline Reservation DBMS/FIles/Group 5 - PHP & HTML Files for DB/input_ticket_form.php
+++ Projects/Web-enabled Database Systems/Airline Reservation DBMS/FIles/Group 5 - PHP & HTML Files for DB/input_ticket_form.php
@@ -4,7 +4,7 @@
 <a href="index.html">Home</a>
 <div style="width: 20px;"></div>
 <?php
-// Form data
+
 $ticket_id = $_POST['ticket_id'];
 $seat_num = $_POST['seat_num'];
 $date = $_POST['date'];
@@ -14,7 +14,7 @@ $transaction_id = $_POST['transaction_id'];
 $amount = $_POST['amount'];
 $payment_mode = $_POST['payment_mode'];
 
-// Connection DSN
+
 $host = "pluto.hood.edu";
 $dbname = "ajo2db";
 $user = "ajo2";
@@ -24,7 +24,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Use prepared statement with named placeholders :ticket_id, :seat_num, :date, :user_id, :flight_num, :transaction_id, :amount, :payment_mode.
+   
     $sql = "INSERT INTO ticket_payment (ticket_id, seat_num, date, user_id, flight_num, transaction_id, amount, payment_mode) 
             VALUES (:ticket_id, :seat_num, :date, :user_id, :flight_num, :transaction_id, :amount, :payment_mode)";
     $stmt = $conn->prepare($sql);
@@ -38,7 +38,7 @@ try {
     $stmt->bindParam(':amount', $amount);
     $stmt->bindParam(':payment_mode', $payment_mode);
 
-    // Execute the prepared statement
+    
     if ($stmt->execute()) {
         $rows_affected = $stmt->rowCount();
         echo "<h2>".$rows_affected." Reservation Made Successfully!</h2>";
